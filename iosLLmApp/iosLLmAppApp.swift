a312430cf04402f113651b10c078dcbbe8508401
sanjaynela <sanjaynelagadde@outlook.com>
2026-01-26 05:46:37 -0800

    Initial Commit

---
+++ iosLLmApp/iosLLmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosLLmAppApp.swift
+//  iosLLmApp
+//
+//  Created by Sanjay Nelagadde on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct iosLLmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosLLmAppTests/iosLLmAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  iosLLmAppTests.swift
+//  iosLLmAppTests
+//
+//  Created by Sanjay Nelagadde on 1/26/26.
+//
+
+import Testing
+
+struct iosLLmAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
